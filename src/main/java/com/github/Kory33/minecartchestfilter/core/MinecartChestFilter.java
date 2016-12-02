51b51d9b250dc8b06f787dd8ce32e3dfad96a536
Kory33 <konryoitdv@gmail.com>
2016-12-03 00:39:28 +0530

    Added (un)registering of the events

--- src/main/java/com/github/Kory33/minecartchestfilter/core/MinecartChestFilter.java
+++ src/main/java/com/github/Kory33/minecartchestfilter/core/MinecartChestFilter.java
@@ -1,17 +1,29 @@
 package com.github.Kory33.minecartchestfilter.core;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecartChestFilter extends JavaPlugin {
     public static final String FILTERED_MINECART_METAKEY = "cart_inventory_filtering_type";
     
+    private EventInterceptor eventInterceptor; 
+    
     @Override
     public void onEnable(){
-        this.getLogger().info("Loaded MinecartChestFilter. Started Initialization...");
+        this.getLogger().info("Loaded MinecartChestFilter. Started initialization...");
+        this.getLogger().info("Registering the events...");
+        
+        this.eventInterceptor = new EventInterceptor(this);
+        
+        this.getLogger().info("Completed initialization.");
     }
 
     @Override
     public void onDisable(){
-        this.getLogger().info("Unloaded MinecartChestFilter successfully.");
+        this.getLogger().info("Finalizing MinecartChestFilter...");
+
+        HandlerList.unregisterAll(eventInterceptor);
+        
+        this.getLogger().info("Unloaded/unregistered MinecartChestFilter successfully.");
     }
 }
