d77a19d233cdde317e666a1d73634d5173e365e9
Kory33 <konryoitdv@gmail.com>
2016-12-03 00:19:51 +0530

    Added detailed flow to onPlayerInteract

--- src/main/java/com/github/Kory33/minecartchestfilter/core/EventInterceptor.java
+++ src/main/java/com/github/Kory33/minecartchestfilter/core/EventInterceptor.java
@@ -1,5 +1,8 @@
 package com.github.Kory33.minecartchestfilter.core;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.minecart.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -11,8 +14,29 @@ class EventInterceptor implements Listener {
         pManager.registerEvents(this, plugin);
     }
     
+    
     @EventHandler
     public void onPlayerInteract(PlayerInteractEntityEvent event){
-        if ()
+        Entity clickedEntity = event.getRightClicked();
+        
+        // if the clicked entity is a storage minecart
+        if (clickedEntity instanceof StorageMinecart){
+            onPlayerInteractWithStorageMinecart(event);
+        }
+        
+        return;
+    }
+    
+    
+    private void onPlayerInteractWithStorageMinecart(PlayerInteractEntityEvent event) {
+        Minecart clickedMinecart = (Minecart) event.getRightClicked();
+        
+        // check if the minecart has the metadata
+        if (!clickedMinecart.hasMetadata(MinecartChestFilter.FILTERED_MINECART_METAKEY)){
+            return;
+        }
+        
+        event.setCancelled(true);
+        return;
     }
 }
