26d46e8ccb8065c3e4b749d64a3a352384718ae6
Kory33 <konryoitdv@gmail.com>
2016-12-03 10:03:15 +0530

    added explanation to a method

--- src/main/java/com/github/Kory33/minecartchestfilter/util/inventory/FilteredInventoryUtil.java
+++ src/main/java/com/github/Kory33/minecartchestfilter/util/inventory/FilteredInventoryUtil.java
@@ -4,7 +4,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.minecart.StorageMinecart;
 
 public class FilteredInventoryUtil {
-
+    /**
+     * Open a restricted inventory for player, that is associated with clickedMinecart
+     * @param player To whom the inventory is shown
+     * @param clickedMinecart To which the filtered inventory belongs
+     */
     public static void openFilteredInventory(Player player, StorageMinecart clickedMinecart) {
         // TODO implementation
     }
