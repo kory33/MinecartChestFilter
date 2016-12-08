7c6f617018910c07e0f4b8fc7d8224753e76e473
Kory33 <konryoitdv@gmail.com>
2016-12-08 12:53:09 +0530

    added FuelFilter

---
+++ src/main/java/com/github/Kory33/minecartchestfilter/filter/FuelFilter.java
@@ -0,0 +1,19 @@
+package com.github.Kory33.minecartchestfilter.filter;
+
+import org.bukkit.craftbukkit.v1_10_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Filter that only allows fuel items.
+ * Implementation utilizes version-dependent methods.
+ * @author Kory33
+ */
+public class FuelFilter extends Filter {
+
+    @Override
+    public boolean isItemAllowed(ItemStack itemStack) {
+        net.minecraft.server.v1_10_R1.ItemStack nmItemStack = CraftItemStack.asNMSCopy(itemStack);
+        return net.minecraft.server.v1_10_R1.TileEntityFurnace.isFuel(nmItemStack);
+    }
+
+}
